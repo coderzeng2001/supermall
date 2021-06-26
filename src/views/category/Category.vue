93fa4b44141e3eb526d789061de46b94f7265270
codezeng <codezeng2001@163.com>
2021-06-26 14:27:00 +0800

    初始化项目

---
+++ src/views/category/Category.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="">
+   <h2>分类</h2>
+ </div>
+</template>
+ 
+<script>
+export default {
+ name: 'Category',
+}
+</script>
+ 
+<style scoped>
+ 
+</style>
---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="">
+   <h2>个人</h2>
+ </div>
+</template>
+ 
+<script>
+export default {
+ name: 'Profile',
+}
+</script>
+ 
+<style scoped>
+ 
+</style>
